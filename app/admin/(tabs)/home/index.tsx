a095fbfb35d3e6fff61b2b0e21fb3c41a341720c
Your Name <yourmail@mail.com>
2024-12-14 10:01:14 +0800

    fixed: message and admin icon

--- app/admin/(tabs)/home/index.tsx
+++ app/admin/(tabs)/home/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
   const router = useRouter()
   return (
     <View className="flex-1 p-10">
-      <View className="w-full bg-green-200 rounded-xl p-5 flex flex-row gap-10 items-center justify-center">
+      <View className="w-full mt-5 bg-green-200 rounded-xl p-5 flex flex-row gap-10 items-center justify-center">
         <View>
           <Feather name="users" color={'green'} size={28} />
           <Text>24</Text>
@@ -26,7 +26,7 @@ const index = () => {
           className="flex items-center w-1/4 aspect-square"
           onPress={() => router.push('/admin/home/manage-posts')}
         >
-          <View className="flex flex-col items-center bg-green-400 p-8 rounded-2xl ">
+          <View className="flex flex-col items-center bg-green-400 p-5 rounded-2xl ">
             <Feather name="home" color="white" size={28} />
           </View>
           <Text className="text-xs text-slate-400">Manage Post</Text>
@@ -35,7 +35,7 @@ const index = () => {
           className="flex items-center w-1/4 aspect-square"
           onPress={() => router.push('/admin/home/manage-users')}
         >
-          <View className="flex flex-col items-center bg-green-400 p-8 rounded-2xl ">
+          <View className="flex flex-col items-center bg-green-400 p-5 rounded-2xl ">
             <Feather name="users" color={'white'} size={28} />
           </View>
           <Text className="text-xs text-slate-400">Manage Users</Text>
@@ -44,7 +44,7 @@ const index = () => {
           className="flex items-center w-1/4 aspect-square"
           onPress={() => router.push('/admin/home/manage-course')}
         >
-          <View className="flex flex-col items-center bg-green-400 p-8 rounded-2xl ">
+          <View className="flex flex-col items-center bg-green-400 p-5 rounded-2xl ">
             <Feather name="edit" color={'white'} size={28} />
           </View>
           <Text className="text-xs text-slate-400">Manage Course</Text>
@@ -53,7 +53,7 @@ const index = () => {
           className="flex items-center w-1/4 aspect-square"
           onPress={() => router.push('/admin/(tabs)/home/manage-year-level')}
         >
-          <View className="flex flex-col items-center bg-green-400 p-8 rounded-2xl ">
+          <View className="flex flex-col items-center bg-green-400 p-5 rounded-2xl ">
             <Feather name="edit-2" color={'white'} size={28} />
           </View>
           <Text className="text-xs text-slate-400">Manage Year</Text>
