a8e0e30f846984c41f0c0324d36d8c9fac776a51
Your Name <yourmail@mail.com>
2025-02-01 17:06:15 +0800

    sd

--- metro.config.js
+++ metro.config.js
@@ -3,29 +3,8 @@ const { withNativeWind } = require('nativewind/metro')
 
 const config = getDefaultConfig(__dirname)
 
+config.resolver.assetExts.push('tflite')
+
+// NOTE: Update this to include the paths to all of your component files.
+
 module.exports = withNativeWind(config, { input: './global.css' })
-
-// const { getDefaultConfig } = require('expo/metro-config')
-// const { withNativeWind } = require('nativewind/metro')
-// const {
-//   resolver: { sourceExts, assetExts },
-// } = require('metro-config')
-
-// const config = getDefaultConfig(__dirname)
-
-// module.exports = withNativeWind(config, {
-//   input: './global.css',
-//   resolver: {
-//     assetExts: assetExts.filter((ext) => ext !== 'svg'),
-//     sourceExts: [...sourceExts, 'svg'],
-//   },
-//   transformer: {
-//     babelTransformerPath: require.resolve('react-native-svg-transformer'),
-//     getTransformOptions: async () => ({
-//       transform: {
-//         experimentalImportSupport: false,
-//         inlineRequires: true,
-//       },
-//     }),
-//   },
-// })
