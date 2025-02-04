b61c6ab91208f00f056a9a524d2220e3621fc3c3
Your Name <yourmail@mail.com>
2025-02-04 16:51:35 +0800

    add some design

--- components/user/ProfileHeader.tsx
+++ components/user/ProfileHeader.tsx
@@ -17,21 +17,19 @@ const ProfileHeader = ({ user, pickImage, id }: IProps) => {
   const { postsCount, likesCount, dislikesCount } = useProfile(id)
 
   return (
-    <>
+    <View className="relative mb-56 ">
       <View
         style={{
           height: 200,
           width: '100%',
-          // marginTop: 10,
-          // shadowOffset: { width: 0, height: 2 },
+          padding: 5,
         }}
-        className="bg-white shadow"
       >
         {user?.coverImage && user?.coverImage !== 'undefined' ? (
           <Image
             style={{ width: '100%', height: '100%' }}
             source={{ uri: user?.coverImage }}
-            resizeMode="contain"
+            resizeMode="cover"
           />
         ) : (
           <View className=" mt-20 items-center justify-center">
@@ -69,74 +67,74 @@ const ProfileHeader = ({ user, pickImage, id }: IProps) => {
         )}
       </View>
       <View
-        className="relative bg-black "
+        className=" top-56 absolute w-full shadow "
         style={{
-          borderTopRightRadius: 30,
-          borderTopLeftRadius: 30,
+          borderTopRightRadius: 40,
+          borderTopLeftRadius: 40,
+          height: 280,
         }}
-      >
-        <View className=" relative flex flex-col gap-5 items-center">
-          <View className="h-36 w-36 rounded-full items-center justify-center border-4 overflow-hidden border-gray-300">
-            {user?.avatar && user.avatar !== 'undefined' ? (
-              <Image
-                style={{ width: '100%', height: '100%' }}
-                resizeMode="cover"
-                source={{ uri: user?.avatar }}
-              />
-            ) : (
-              <Feather name="user" size={60} color="gray" />
-            )}
-          </View>
+      />
+      <View className=" absolute w-full top-44 justify-center flex flex-col gap-5 items-center">
+        <View className="h-36 w-36 rounded-full items-center justify-center border-4 overflow-hidden border-gray-300">
+          {user?.avatar && user.avatar !== 'undefined' ? (
+            <Image
+              style={{ width: '100%', height: '100%' }}
+              resizeMode="cover"
+              source={{ uri: user?.avatar }}
+            />
+          ) : (
+            <Feather name="user" size={60} color="gray" />
+          )}
+        </View>
 
-          <Text className="text-2xl font-semibold first-letter:uppercase">
-            {user?.name}
-          </Text>
-          <View className="flex flex-row gap-10">
-            <View className="flex flex-col items-center">
-              <Text className="font-bold text-2xl">{postsCount}</Text>
-              <Text>Posts</Text>
-            </View>
-            <View className="flex flex-col items-center">
-              <Text className="font-bold text-2xl">{likesCount}</Text>
-              <Text>Likes</Text>
-            </View>
-            <View className="flex flex-col items-center">
-              <Text className="font-bold text-2xl">{dislikesCount}</Text>
-              <Text>Dislike</Text>
-            </View>
+        <Text className="text-2xl font-semibold first-letter:uppercase">
+          {user?.name}
+        </Text>
+        <View className="flex flex-row gap-10">
+          <View className="flex flex-col items-center">
+            <Text className="font-bold text-2xl">{postsCount}</Text>
+            <Text>Posts</Text>
           </View>
-          <View className="flex flex-row gap-5">
-            {id === auth?.currentUser?.uid && (
-              <Link href="/user/(tabs)/settings/edit-profile" asChild>
-                <TouchableOpacity className="bg-green-500 px-5 py-2 rounded-xl flex flex-row items-center gap-5">
-                  <Ionicons name="create" size={24} color="white" />
-                  <Text className="text-lg font-semibold text-white">
-                    Edit Profile
-                  </Text>
-                </TouchableOpacity>
-              </Link>
-            )}
-
-            {id !== auth?.currentUser?.uid && user && (
-              <Link
-                href={{
-                  pathname: `/user/(tabs)/messages/conversations/user`,
-                  params: {
-                    id: user._id,
-                  },
-                }}
-                asChild
-              >
-                <TouchableOpacity className="bg-green-200 px-5 py-2 rounded-xl flex flex-row items-center gap-5">
-                  <Feather name="message-square" size={24} color="black" />
-                  <Text className="text-lg font-semibold">Message</Text>
-                </TouchableOpacity>
-              </Link>
-            )}
+          <View className="flex flex-col items-center">
+            <Text className="font-bold text-2xl">{likesCount}</Text>
+            <Text>Likes</Text>
+          </View>
+          <View className="flex flex-col items-center">
+            <Text className="font-bold text-2xl">{dislikesCount}</Text>
+            <Text>Dislike</Text>
           </View>
         </View>
+        <View className="flex flex-row gap-5">
+          {id === auth?.currentUser?.uid && (
+            <Link href="/user/(tabs)/settings/edit-profile" asChild>
+              <TouchableOpacity className="bg-green-500 px-5 py-2 rounded-xl flex flex-row items-center gap-5">
+                <Ionicons name="create" size={24} color="white" />
+                <Text className="text-lg font-semibold text-white">
+                  Edit Profile
+                </Text>
+              </TouchableOpacity>
+            </Link>
+          )}
+
+          {id !== auth?.currentUser?.uid && user && (
+            <Link
+              href={{
+                pathname: `/user/(tabs)/messages/conversations/user`,
+                params: {
+                  id: user._id,
+                },
+              }}
+              asChild
+            >
+              <TouchableOpacity className="bg-green-200 px-5 py-2 rounded-xl flex flex-row items-center gap-5">
+                <Feather name="message-square" size={24} color="black" />
+                <Text className="text-lg font-semibold">Message</Text>
+              </TouchableOpacity>
+            </Link>
+          )}
+        </View>
       </View>
-    </>
+    </View>
   )
 }
 
