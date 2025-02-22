a355311aad9c34b040a588009dfc6b467369399d
Your Name <yourmail@mail.com>
2025-02-22 11:01:35 +0800

    chore: add scrollview in manage user

--- app/admin/(tabs)/home/manage-users.tsx
+++ app/admin/(tabs)/home/manage-users.tsx
@@ -5,7 +5,14 @@ import { Feather } from '@expo/vector-icons'
 import { Stack } from 'expo-router'
 import { doc, DocumentData, updateDoc } from 'firebase/firestore'
 import React, { useEffect, useState } from 'react'
-import { Alert, Image, Text, TouchableOpacity, View } from 'react-native'
+import {
+  Alert,
+  Image,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native'
 
 const manageUsers = () => {
   const [filteredUsers, setFilteredUsers] = useState<DocumentData[]>([])
@@ -83,7 +90,7 @@ const manageUsers = () => {
       style={{
         flex: 1,
         padding: 10,
-        paddingTop: 35,
+        // paddingTop: 35,
         gap: 10,
         backgroundColor: 'white',
       }}
@@ -99,64 +106,66 @@ const manageUsers = () => {
         }}
       />
 
-      {filteredUsers?.map((user: DocumentData) => {
-        return (
-          <View className="flex items-start gap-5 flex-row justify-between">
-            <View className="flex items-center flex-row gap-5">
-              <View
-                style={{
-                  height: 64,
-                  width: 64,
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                  borderWidth: 1,
-                  borderRadius: 50,
-                  // padding: 16,
-                }}
-              >
-                {user.avatar && user.avatar !== 'undefined' ? (
-                  <Image
-                    source={{ uri: user.avatar }}
-                    alt="avatar"
-                    style={{
-                      width: '100%',
-                      height: '100%',
-                      borderRadius: 50,
-                    }}
-                  />
-                ) : (
-                  <Feather name="user" size={24} />
-                )}
-              </View>
+      <ScrollView style={{ flex: 1, marginBottom: 60 }}>
+        {filteredUsers?.map((user: DocumentData) => {
+          return (
+            <View className="flex items-start gap-5 flex-row justify-between">
+              <View className="flex items-center flex-row gap-5">
+                <View
+                  style={{
+                    height: 64,
+                    width: 64,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    borderWidth: 1,
+                    borderRadius: 50,
+                    // padding: 16,
+                  }}
+                >
+                  {user.avatar && user.avatar !== 'undefined' ? (
+                    <Image
+                      source={{ uri: user.avatar }}
+                      alt="avatar"
+                      style={{
+                        width: '100%',
+                        height: '100%',
+                        borderRadius: 50,
+                      }}
+                    />
+                  ) : (
+                    <Feather name="user" size={24} />
+                  )}
+                </View>
 
-              <View className="flex flex-col gap-2 ">
-                <Text>{user.name}</Text>
-                <Text style={{ fontSize: 10 }}>{user.email}</Text>
-                <View className="flex justify-between items-center flex-row gap-2 ">
-                  <TouchableOpacity
-                    className="bg-green-500 px-16 py-2 rounded-xl"
-                    onPress={() => sendResetEmail(user.email)}
-                  >
-                    <Text className="text-white">Reset</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    className={`${user.frozen ? 'bg-red-300' : 'bg-red-500'} px-12 py-2 rounded-xl`}
-                    onPress={
-                      user.frozen
-                        ? () => handleUnFrozen(user._id)
-                        : () => handleFrozen(user._id)
-                    }
-                  >
-                    <Text className="text-white">
-                      {user.frozen ? 'Unfreeze' : 'Frozen'}
-                    </Text>
-                  </TouchableOpacity>
+                <View className="flex flex-col gap-2 ">
+                  <Text>{user.name}</Text>
+                  <Text style={{ fontSize: 10 }}>{user.email}</Text>
+                  <View className="flex justify-between items-center flex-row gap-2 ">
+                    <TouchableOpacity
+                      className="bg-green-500 px-16 py-2 rounded-xl"
+                      onPress={() => sendResetEmail(user.email)}
+                    >
+                      <Text className="text-white">Reset</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      className={`${user.frozen ? 'bg-red-300' : 'bg-red-500'} px-12 py-2 rounded-xl`}
+                      onPress={
+                        user.frozen
+                          ? () => handleUnFrozen(user._id)
+                          : () => handleFrozen(user._id)
+                      }
+                    >
+                      <Text className="text-white">
+                        {user.frozen ? 'Unfreeze' : 'Frozen'}
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
                 </View>
               </View>
             </View>
-          </View>
-        )
-      })}
+          )
+        })}
+      </ScrollView>
     </View>
   )
 }
